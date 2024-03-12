d235cb6e5818396df808239ee1e291d93f96d5ba
Charlize <extrabutterychar@gmail.com>
2024-03-12 14:49:21 -0700

    Removing Physics Body

--- src/prefabs/Projectile.js
+++ src/prefabs/Projectile.js
@@ -4,8 +4,6 @@ class Projectile extends Phaser.Physics.Arcade.Sprite{
         // Note: might add health decrease in projectile
         // projectile will spawn on player
         scene.add.existing(this)
-        scene.physics.add.existing(this)
-
         this.moveSpeed = 1
         this.character = character
         this.startX = this.x
@@ -14,8 +12,8 @@ class Projectile extends Phaser.Physics.Arcade.Sprite{
     }
 
     move(landX, landY) {
-        console.log('enemyX: ' + landX + 'enemyY:' + landY)
-        console.log('thisX: ' + this.x + 'thisY:' + this.y)
+        // console.log('enemyX: ' + landX + 'enemyY:' + landY)
+        // console.log('thisX: ' + this.x + 'thisY:' + this.y)
         // this.setVisible(true)
         if(this.x >= landX){
             this.anims.play(`${this.character.name}_projectileAttack`)
@@ -23,12 +21,12 @@ class Projectile extends Phaser.Physics.Arcade.Sprite{
             // increase if going to the right
             // decrease if going to the left
             if (landX > this.x){
-                // this.x += this.moveSpeed
-                this.setVelocityX(this.moveSpeed)
+                this.x += this.moveSpeed
+                // this.body.setVelocityX(this.moveSpeed)
             }
             if(landX < this.x){
-                // this.x -= this.moveSpeed
-                this.setVelocityX(this.moveSpeed)
+                this.x -= this.moveSpeed
+                // this.body.setVelocityX(this.moveSpeed)
             }
         }
         if (this.x == landX){
